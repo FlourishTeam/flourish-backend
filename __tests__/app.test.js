c5e85a7168bf8e9c0273789eb58634fb1efa17a1
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-04 14:26:07 -0800

    add users table (#1)
    * add users table
    
    * passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,10 @@ const app = require('../lib/app');
 
 describe('flourish-backend routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('passes a test', () => {
+    expect(true).toEqual(true);
   });
 });
