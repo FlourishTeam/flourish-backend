200ea40d980b89d5e655688ec6114f0b133bcf9a
Jena Boehm <68626277+jena-boehm@users.noreply.github.com>
2021-03-04 18:15:22 -0800

    User model/card 1 testing crud routes (#5)
    * add ensure-auth middleware
    
    * add auth signup, login, verify routes
    
    * add auth route to app.js
    
    * passing dummy tests
    
    * signup via POST test passing
    
    * login and verify tests passing
    
    * remove console logs
    
    * update yml

---
+++ __tests__/auth.test.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+describe('auth routes', () => {
+  let agent;
+  let user;
+
+  beforeEach(async() => {
+    await pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+
+    agent = await request.agent(app);
+    user = await UserService.create({
+      email: 'test@test.com',
+      password: 'password', 
+      name: 'Name'
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('allows the user to signup via POST', async() => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password', 
+        name: 'Name'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          email: 'test@test.com',
+          name: 'Name'
+        });
+      });
+  });
+
+  it('allows the user to login via POST', async() => {
+    const res = await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'test@test.com',
+      name: 'Name'
+    });
+  });
+
+  it('verifies a user is logged in', async() => {
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+
+    const res = await agent 
+      .get('/api/v1/auth/verify');
+
+    expect(res.body).toEqual({
+      id: user.id,
+      email: 'test@test.com',
+      name: 'Name'
+    });
+  });
+});
