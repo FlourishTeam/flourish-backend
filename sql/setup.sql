aeeb297cca47347a9f933b48b813392dd1d03242
jeffIrvine <jirvine1212@gmail.com>
2021-03-07 14:44:34 -0800

    added new columns to plant and user_plants tables, added plants table, and fixed UserPlants model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS plants CASCADE; 
+DROP TABLE IF EXISTS user_plants CASCADE; 
 
 CREATE TABLE users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,18 +10,53 @@ CREATE TABLE users(
 )
 
 CREATE TABLE plants(
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT REFEENCES users(user_id),
+    plant_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    image TEXT NOT NULL,
     common_name TEXT NOT NULL,
+    synonyms TEXT NOT NULL,
     scientific_name TEXT NOT NULL,
-    light_rang TEXT NOT NULL,
+    light_range TEXT NOT NULL,
     hydration_range TEXT NOT NULL,
     care_difficulty TEXT NOT NULL,
+    pests_diseases TEXT NOT NULL,
+    warnings TEXT NOT NULL,
+    height TEXT NOT NULL,
+    spread TEXT NOT NULL,
+    type TEXT NOT NULL,
+    flowering_period TEXT NOT NULL,
+    bloom_size TEXT NOT NULL,
     temperature_range TEXT NOT NULL,
     placement TEXT NOT NULL,
     humidity_level TEXT NOT NULL,
     substrate_recommendation TEXT NOT NULL,
     potting_notes TEXT NOT NULL,
-    watering TEXT NOT NULL
+    watering TEXT NOT NULL,
+    propagation TEXT NOT NULL
+);
+
+CREATE TABLE user_plants(
+    user_plant_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFEENCES users(id),
+    image TEXT NOT NULL,
+    common_name TEXT NOT NULL,
+    synonyms TEXT NOT NULL,
+    scientific_name TEXT NOT NULL,
+    light_range TEXT NOT NULL,
+    hydration_range TEXT NOT NULL,
+    care_difficulty TEXT NOT NULL,
+    pests_diseases TEXT NOT NULL,
+    warnings TEXT NOT NULL,
+    height TEXT NOT NULL,
+    spread TEXT NOT NULL,
+    type TEXT NOT NULL,
+    flowering_period TEXT NOT NULL,
+    bloom_size TEXT NOT NULL,
+    temperature_range TEXT NOT NULL,
+    placement TEXT NOT NULL,
+    humidity_level TEXT NOT NULL,
+    substrate_recommendation TEXT NOT NULL,
+    potting_notes TEXT NOT NULL,
+    watering TEXT NOT NULL,
+    propagation TEXT NOT NULL
 );
 
