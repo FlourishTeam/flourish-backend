4d8f6a4aad7b070c00332aca21271639441480e5
jeffIrvine <jirvine1212@gmail.com>
2021-03-06 22:18:49 -0800

    wrote plants table, added user_id to model, and changed get by id and delete

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS users CASCADE; 
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS plants CASCADE; 
 
 CREATE TABLE users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,3 +7,20 @@ CREATE TABLE users(
     password_hash TEXT NOT NULL,
     name TEXT NOT NULL
 )
+
+CREATE TABLE plants(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFEENCES users(user_id),
+    common_name TEXT NOT NULL,
+    scientific_name TEXT NOT NULL,
+    light_rang TEXT NOT NULL,
+    hydration_range TEXT NOT NULL,
+    care_difficulty TEXT NOT NULL,
+    temperature_range TEXT NOT NULL,
+    placement TEXT NOT NULL,
+    humidity_level TEXT NOT NULL,
+    substrate_recommendation TEXT NOT NULL,
+    potting_notes TEXT NOT NULL,
+    watering TEXT NOT NULL
+);
+
