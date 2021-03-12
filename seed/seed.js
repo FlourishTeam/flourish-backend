68e90c5bd501785e1bc0c5af3b618317fd22eba2
Dee Watson <45681154+dl-watson@users.noreply.github.com>
2021-03-12 10:15:48 -0800

    cleaning up backend, caught one error in return from remove log by lo… (#43)
    * cleaning up backend, caught one error in return from remove log by log id
    
    * remove console.log
    
    * tidied up the backend

--- seed/seed.js
+++ seed/seed.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const pool = require("../lib/utils/pool");
-const Data = require("./mock.json");
+require('dotenv').config();
+const pool = require('../lib/utils/pool');
+const Data = require('./mock.json');
 
 seedFunction();
 
@@ -9,32 +9,29 @@ async function seedFunction() {
     await Promise.all(
       Data.map(async (seed) => {
         await pool.query(
-          `
-      INSERT INTO plants
-      (image,  
-      common_name, 
-      synonyms, 
-      scientific_name, 
-      light_range, 
-      hydration_range, 
-      care_difficulty,
-      pests_diseases,
-      warnings, 
-      height, 
-      spread, 
-      type, 
-      flowering_period, 
-      bloom_size, 
-      temperature_range, 
-      placement, 
-      humidity_level, 
-      substrate_recommendation, 
-      potting_notes, 
-      watering, 
-      propagation) 
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)
-      
-      `,
+          `INSERT INTO plants
+              (image,  
+              common_name, 
+              synonyms, 
+              scientific_name, 
+              light_range, 
+              hydration_range, 
+              care_difficulty,
+              pests_diseases,
+              warnings, 
+              height, 
+              spread, 
+              type, 
+              flowering_period, 
+              bloom_size, 
+              temperature_range, 
+              placement, 
+              humidity_level, 
+              substrate_recommendation, 
+              potting_notes, 
+              watering, 
+              propagation) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`,
           [
             seed.image,
             seed.common_name,
